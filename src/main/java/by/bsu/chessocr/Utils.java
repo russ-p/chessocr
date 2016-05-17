4db3bce1fb9e2bd7ea050f1b616c0bbfd0c7720d
russ <russ@localhost>
2016-05-17 21:58:15 +0300

    ???????  ?????? ??? ???????????

--- src/main/java/by/bsu/chessocr/Utils.java
+++ src/main/java/by/bsu/chessocr/Utils.java
@@ -1,9 +1,9 @@
 package by.bsu.chessocr;
 
 import static org.bytedeco.javacpp.opencv_imgproc.COLOR_BGR2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.COLOR_RGB2HSV;
 import static org.bytedeco.javacpp.opencv_imgproc.THRESH_BINARY;
 import static org.bytedeco.javacpp.opencv_imgproc.THRESH_OTSU;
-import static org.bytedeco.javacpp.opencv_imgproc.*;
 import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
 import static org.bytedeco.javacpp.opencv_imgproc.threshold;
 
@@ -15,11 +15,15 @@ import javax.imageio.ImageIO;
 
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.Rect;
+import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameConverter.ToMat;
 
 public class Utils {
+
+	private static final Java2DFrameConverter frameConverter = new Java2DFrameConverter();
+	private static final ToMat toMatConverter = new OpenCVFrameConverter.ToMat();
 
 	public static Mat toBW(Mat src) {
 		// to Grayscale
@@ -37,7 +41,7 @@ public class Utils {
 		cvtColor(src, img_gray, COLOR_BGR2GRAY);
 		return img_gray;
 	}
-	
+
 	public static Mat toHSV(Mat src) {
 		Mat img_out = src.clone();
 		cvtColor(src, img_out, COLOR_RGB2HSV);
@@ -45,30 +49,35 @@ public class Utils {
 	}
 
 	public static Mat convertBuffToMat(BufferedImage img) {
-		Java2DFrameConverter converter1 = new Java2DFrameConverter();
-		ToMat converter2 = new OpenCVFrameConverter.ToMat();
-		Mat iploriginal = converter2.convert(converter1.convert(img));
+		Frame frame = frameConverter.getFrame(img, 1.0, true);
+		Mat iploriginal = toMatConverter.convert(frame);
 		return iploriginal;
 	}
 
 	public static BufferedImage convertMatToBuff(Mat img) {
-		Java2DFrameConverter converter1 = new Java2DFrameConverter();
-		OpenCVFrameConverter.ToMat converter2 = new OpenCVFrameConverter.ToMat();
-		BufferedImage buff = converter1.convert(converter2.convert(img));
+		BufferedImage buff = frameConverter.convert(toMatConverter.convert(img));
 		return buff;
 	}
-
+	
 	public static Mat normalize(Mat src) {
 		Mat dst = new Mat(src.size(), src.type());
 		org.bytedeco.javacpp.opencv_core.normalize(src, dst);
 		return dst;
 	}
 
-	public static void writeTestImage(String filename, Mat region) {
+	public static void writeTestImage2(String filename, Mat region) {
 		BufferedImage image = Utils.convertMatToBuff(region);
 		try {
 			ImageIO.write(image, "PNG", new File("out-imgs/" + filename + ".png"));
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void writeTestImage(String filename, Mat region) {
+		try {
+			org.bytedeco.javacpp.opencv_highgui.imwrite("out-imgs/" + filename + ".png", region);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
